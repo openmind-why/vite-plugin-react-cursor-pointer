06a9b40aaece8f7b5c8599541a3c66f23b2c2805
wanghongyang <549492949@qq.com>
2024-12-28 16:14:29 +0800

    ✨ feat: init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # vite-plugin-react-cursor-pointer
 this is an quick increase cursor pointer result vite puligin
+
+
+
+<div __cursorPointer>this is example , __cursorPointer attributes</div>
+
+// will be transform to 
+<div style='cursor:pointer'></div>
+
+// if you have TS , you need increase statement (this is a react example):
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    __cursorPointer?: boolean
+  }
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+export default () => {
+  return {
+    name: 'vite-plugin-react-cursor-pointer-loader',
+    enforce: 'pre',
+    transform(code, id) {
+
+      if (id.includes('.tsx')) {
+        let tCode = code
+        const diyRegex = /<[^>]*\b__cursorPointer\b[^>]*>/g;
+        const cursorPointer = tCode.match(diyRegex);
+
+        if (cursorPointer) {
+          const styleRegex = /style=\{\{([^{}]*(?:\{[^{}]*(?:\{[^{}]*(?:\{[^{}]*(?:\{[^{}]*\}[^{}]*)*\}[^{}]*)*\}[^{}]*)*\}[^{}]*)*)\}\}/g;
+
+          cursorPointer.forEach(match => {
+
+            const style = match.match(styleRegex)?.[0];
+
+            if (style) {
+              const styleValueRegex = /\{([^{}]*(?:\{[^{}]*(?:\{[^{}]*(?:\{[^{}]*\}[^{}]*)*\}[^{}]*)*\}[^{}]*)*)\}/
+              let tStyle = style.replace(styleValueRegex, (_, value) => ` {{...${value},cursor:'pointer'}}`)
+              tCode = tCode.replace(style, tStyle)
+            } else {
+              tCode = tCode.replace('__cursorPointer', `style={{cursor:'pointer'}}`)
+            }
+          });
+        }
+        return tCode
+      }
+    },
+
+  }
+
+}
